ad1e624406b65060e78f9f5d53ab7571706d35e1
Kishimi <anton@green-pr.org>
2021-09-13 20:05:46 +0200

    v1.0.1 => v1.1.0

--- source/Version.hpp
+++ source/Version.hpp
@@ -3,8 +3,8 @@
 #include <string>
 
 #define BR4IN_VERSION_MAJOR 1
-#define BR4IN_VERSION_MINOR 0
-#define BR4IN_VERSION_PATCH 1
+#define BR4IN_VERSION_MINOR 1
+#define BR4IN_VERSION_PATCH 0
 
 namespace Br4in
 {
